2119a502b19abefdfe0c1cb280e8a5f42980e7bf
mateusz.pierzchala <mateusz.pierzchala1@gmail.com>
2019-02-24 21:57:15 +0100

    GET_DATA

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-print(__name__)
+books = [
+    {
+        'name': 'Władca Pierścieni: Drużyna Pierścienia',
+        'price': 34.50,
+        'isbn':9788375062182
+    },
+    {
+        'name': 'Lew, czarownica i stara szafa',
+        'price': 19.99,
+        'isbn':9788375062183
+    }
+]
+#GET /books/9788375062182
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+#GET /books
+@app.route('/books')
+def get_books():
 
-app.run(port=5000)
+    return jsonify({'books':books})
+
+@app.route ('/books/<int:isbn>')
+def get_book_by_isbn(isbn):
+    return_value = {}
+    for book in books:
+        if book["isbn"] == isbn:
+            return_value = {
+                'name': book["name"],
+                'price': book["price"]
+            }
+    return jsonify(return_value)
+
+app.run(port=5000) 
