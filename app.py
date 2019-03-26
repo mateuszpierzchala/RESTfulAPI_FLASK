86b476e07686a49aa2ecd0a8dc059c8ef470c01e
mateusz.pierzchala <mateusz.pierzchala1@gmail.com>
2019-03-26 21:58:48 +0100

    ADD & PUT

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, json, request, Response, jsonify
 
 app = Flask(__name__)
 books = [
@@ -31,7 +31,7 @@ def get_books():
 #}
 
 def validBookObject(bookObject):
-    if ("name" in dictionaryObject and "price" in bookObject and "isbn" in bookObject):
+    if ("name" in bookObject and "price" in bookObject and "isbn" in bookObject):
         return True
     else:
         return False 
@@ -41,10 +41,23 @@ def validBookObject(bookObject):
 def add_book():
     request_data = request.get_json()
     if(validBookObject(request_data)):
-       books.insert(0,request_data)
-       return "True"
+        new_book = {
+            "name": request_data['name'],
+            "price": request_data['price'],
+            "isbn": request_data['isbn']
+        }
+
+        books.insert(0,new_book)
+        response = Response("", 201, mimetype='application/json')
+        response.headers['Location'] = "/books/" + str(new_book['isbn'])
+        return response
     else:
-       return "False"
+        invalidBookObjectErrorMsg = {
+            "error":"proba przeslannia nieprawidlowego obiektu",
+            "helpString": "prawidlowy format: {'name':'bookname', 'price': 7.89, 'isbn':'isbn_number'}"
+        }
+        response = Response(json.dumps(invalidBookObjectErrorMsg), status=400, mimetype='application/json');
+        return response
 
 @app.route ('/books/<int:isbn>')
 def get_book_by_isbn(isbn):
@@ -56,5 +69,24 @@ def get_book_by_isbn(isbn):
                 'price': book["price"]
             }
     return jsonify(return_value)
+
+#PUT /books/
+@app.route('/books/<int:isbn>', methods = ['PUT'])
+def replace_book(isbn):
+    request_data = request.get_json()
+    new_book = {
+         'name': book["name"],
+         'price': book["price"],
+         'isbn': isbn
+    }
+    i=0;
+    for book in books:
+        currentIsbn = book["isbn"]
+        if currentIsbn == isbn:
+            books[i] = new_book
+        i +=1
+    response = Response("", status = 204)
+
+
 
 app.run(port=5000) 
